bc344bcf2b736c5d879fcf3f1c6dc4fa9a9e4797
LuisFillype <l.fillype@gmail.com>
2022-03-03 19:35:09 -0300

    feat: add exercicio3

---
+++ src/main/java/Exercicio3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner valorEntrada = new Scanner(System.in);
+        int npa;
+        int np3;
+
+        System.out.println("Nota da prova: ");
+
+        npa = valorEntrada.nextInt();
+        if (npa>=60){
+            System.out.println("Aprovado");
+        }
+        else if (npa<30){
+            System.out.println("Reprovado");
+        }
+        else{
+            System.out.println("Nota NP3: ");
+
+            np3 = valorEntrada.nextInt();
+
+            if ((np3+npa)/2>=50) {
+                System.out.println("Aprovado");
+            }
+            else {
+                System.out.println("Reprovado");
+            }
+
+        }
+    }
+}
