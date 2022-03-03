a2311e298b4bd4866ca7a7476c1d231897746704
LuisFillype <l.fillype@gmail.com>
2022-03-03 14:04:35 -0300

    feat: add exercicio1

---
+++ src/main/java/Exercicio1.java
@@ -0,0 +1,16 @@
+public class Exercicio1 {
+
+    public static void main(String[] args){
+
+        int numeroZumbis1 = 130;
+        int numeroZumbis2 = 240;
+        int numeroZumbis3 = 320;
+
+        int totalZumbis = numeroZumbis1 + numeroZumbis2 + numeroZumbis3;
+
+        int media = totalZumbis/3;
+
+        System.out.println(totalZumbis);
+        System.out.println(media);
+    }
+}
